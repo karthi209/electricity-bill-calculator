351db93172f0d1cd3d9f8a6dd3a95718b581e7d9
karthik <karthikeyan14june@gmail.com>
2024-06-07 22:23:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/electricity-bill-calculator-frontend/"
+  base: "/electricity-bill-calculator/"
 })
